4c90c9ca804f49878046aebe0e9bb132927f75ff
riizlaah <100104632+riizlaah@users.noreply.github.com>
2025-12-13 07:16:45 +0000

    feat: formula validation pt. 2

--- scripts/function_editor/f_builder.gd
+++ scripts/function_editor/f_builder.gd
@@ -18,11 +18,13 @@ func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	if data is MathDisplayLabel:
 		spacer.hide()
-		if curr_formula[-1] in ["+", "-"]:
+		var last_char = curr_formula[-1]
+		if last_char in ["+", "-"] and data.interpreted_str in ["+", "-"]:
 			handle_plus_minus(data)
 			update_formula()
 			spacer.show()
 			return
+		if handle_zero(data): return
 		if data.type == MathDisplayLabel.Type.OPERATOR: toolbar.curr_operator += 1
 		add_child(data)
 		await get_tree().create_timer(0.1).timeout
@@ -39,16 +41,48 @@ func get_formula_as_text() -> String:
 func validate_syntax(new_char: String):
 	const operator := ["+", "-", "*", "/"]
 	var last_char := get_formula_as_text()[-1]
-	print(last_char, new_char)
 	if last_char in ["+", "-"] and new_char in ["+", "-"]: return true
 	if last_char in operator and new_char in operator: return false
+	if float_not_valid(new_char): return false
+	if num_not_valid(new_char): return false
+	return true
+
+func num_not_valid(new_char: String):
+	var formula = get_formula_as_text() + new_char
+	var res = RegEx.create_from_string(r"[a-z]\d|[a-z][a-z]|\d[a-z]").search(formula)
+	if res: return true
+	return false
+
+func float_not_valid(new_char: String):
+	var formula = get_formula_as_text() + new_char
+	var res = RegEx.create_from_string(r"\.\.|\d+\.\d+\.|\D+\.|\.\D").search(formula)
+	if !res: return false
 	return true
 
 func handle_plus_minus(mdl: MathDisplayLabel):
 	var last_char := get_formula_as_text()[-1]
-	if last_char not in ["-", "+"]: return
 	var new_char := mdl.interpreted_str
+	#if last_char not in ["-", "+"] or new_char not in ["-", "+"]: return
 	get_child(-2).interpreted_str = "-" if (last_char == "+" and new_char == "-") or (last_char == "-" and new_char == "+") else "+"
 	get_child(-2).displayed_str = "-" if (last_char == "+" and new_char == "-") or (last_char == "-" and new_char == "+") else "+"
 
+func handle_zero(data: MathDisplayLabel):
+	var comp = get_last_comp()
+	print(comp)
+	if comp == "0" and data.type == MathDisplayLabel.Type.CONSTANT:
+		get_child(-2).interpreted_str = data.interpreted_str
+		get_child(-2).displayed_str = data.displayed_str
+		update_formula()
+		spacer.show()
+		return true
+	return false
+
 func update_formula(): curr_formula = get_formula_as_text()
+
+func get_last_comp():
+	var last_idx = curr_formula.get_slice_count("+") - 1
+	var comp = curr_formula.get_slice("+", last_idx)
+	for op in ["-", "*", "/"]:
+		last_idx = comp.get_slice_count(op) - 1
+		comp = comp.get_slice(op, last_idx)
+	return comp
--- scripts/function_editor/f_builder_spacer.gd
+++ scripts/function_editor/f_builder_spacer.gd
@@ -1,13 +1,22 @@
 class_name FBUilderSpacer
-extends Control
+extends ColorRect
 
 var parent: FBuilder
 
 func _ready() -> void:
 	parent = get_parent()
+	color = Color.TRANSPARENT
 
 func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
-	return parent._can_drop_data(at_position, data)
+	var can = parent._can_drop_data(at_position, data)
+	if can: color = Color(1,1,1,0.5)
+	else: color = Color(1,0,0,0.5)
+	return can
 
 func _drop_data(at_position: Vector2, data: Variant) -> void:
 	await parent._drop_data(at_position, data)
+
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_DRAG_END:
+			color = Color.TRANSPARENT
